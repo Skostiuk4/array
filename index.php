6345b88cfffb20ce65cb743d16714882a6eaceff
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-03 10:43:59 +0300

    final commit

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
   function mas ($numbers) {
     foreach ($numbers as $number) {
+    echo $number;
     }
   }
   $numbers = array(10, 15, 16, 22, 4);
-  echo mas($numbers);
+  $item = mas ($numbers);
